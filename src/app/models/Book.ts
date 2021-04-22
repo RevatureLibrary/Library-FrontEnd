1af47cf968b256deab318e30892ed400424c0ea0
pgonzalez017 <42748202+pgonzalez017@users.noreply.github.com>
2021-04-22 09:39:23 -0400

    Added User Model

--- src/app/models/Book.ts
+++ src/app/models/Book.ts
@@ -1,10 +1,10 @@
-export class Book {
-    id!:number;
-    title!:string;
-    author!:string;
-    isbn!:number;
-    publisher!:string;
-    condition!:string;
-    bookStatus!:string;
-    departments!:string[];
+export interface Book {
+    id:number;
+    title:string;
+    author:string;
+    isbn:number;
+    publisher:string;
+    condition:string;
+    bookStatus:string;
+    departments:string[];
 }
