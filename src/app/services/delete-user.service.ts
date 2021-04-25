582c1df4d75898c1b2f958f69304f09e251a5e97
Kevin <kwang44@buffalo.edu>
2021-04-25 16:07:44 -0400

    fix some headers issues

--- src/app/services/delete-user.service.ts
+++ src/app/services/delete-user.service.ts
@@ -5,10 +5,6 @@ import { Observable } from "rxjs";
 import { User } from "src/app/components/delete-user/delete-user.component";
 import { ActiveUser } from '../models/ActiveUser';
 
-const headers = new HttpHeaders({
-  "Content-Type": "application/json"
-});
-
 @Injectable({
   providedIn: 'root'
 })
@@ -19,21 +15,36 @@ export class DeleteUserService {
   getAllUsers():Observable<User[]> {
     let user = localStorage.getItem('user');
     let currentUser:ActiveUser = JSON.parse(user!);
-    headers.set("Authorization", "Bearer " + currentUser.token)
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + currentUser.token
+    });
+
     return this.http.get<User[]>("http://18.222.177.219:8080/users", { headers: headers });
   }
 
   searchUsers(username:string):Observable<User[]> {
     let user = localStorage.getItem('user');
     let currentUser:ActiveUser = JSON.parse(user!);
-    headers.set("Authorization", "Bearer " + currentUser.token)
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + currentUser.token
+    });
+
     return this.http.get<User[]>("http://18.222.177.219:8080/users/search=" + username, { headers: headers });
   }
 
   deleteUser(user: User):Observable<User> {
     let aUser = localStorage.getItem('user');
     let currentUser:ActiveUser = JSON.parse(aUser!);
-    headers.set("Authorization", "Bearer " + currentUser.token)
+
+    const headers = new HttpHeaders({
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + currentUser.token
+    });
+
     return this.http.delete<User>("http://18.222.177.219:8080/users/" + user.id, { headers: headers});
   }
 
